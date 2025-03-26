be55becef1e880a9646d57d342f20b54054d5851
sarathkumarps17 <sarathkumarps@outlook.com>
2025-03-26 22:47:10 +0530

    🔖 Version: V1.10

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="w-full p-0 m-0 h-screen flex justify-center items-center">
+        <div className="text-3xl">
+          You are ready to setup the project, If you see the text in 3Xl size
+          and at the center of the screen you are able to use tailwind css
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
